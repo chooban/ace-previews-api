4bab4c4c536aff959b66b6a51bfed3322d5444ff
Ross Hendry <rhendry@googlemail.com>
2018-07-28 17:14:27 +0100

    Works with newer tokens

--- app/routes/profiles.js
+++ app/routes/profiles.js
@@ -2,17 +2,28 @@ const express = require('express');
 const request = require('request-promise');
 const querystring = require('querystring');
 const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
 const config = require('../util/auth0-config');
 
+const secret = jwksRsa.expressJwtSecret({
+  cache: true,
+  rateLimit: true,
+  jwksRequestsPerMinute: 5,
+  jwksUri: `https://${config.domain}/.well-known/jwks.json`
+});
+
 const jwtCheck = jwt({
-  secret: config.secret,
-  audience: config.audience
+  secret,
+  audience: config.audience,
+  issuer: `https://${config.domain}/`,
+  algorithms: ['RS256']
 });
 
 const router = express.Router();
 
 const updateSearches = (req, res, next) => {
+  console.log(`Updating searches for ${req.user.sub}`);
   const headers = {
     url: `https://${config.domain}/api/v2/users/${querystring.escape(req.user.sub)}`,
     headers: {
